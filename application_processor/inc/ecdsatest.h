87f1db9d521be45bfe96c0d3af6ec11d5e52f0fb
Salpingopharyngeus <75758520+Salpingopharyngeus@users.noreply.github.com>
2024-02-16 18:12:47 -0500

    Brute Force Added Headers

---
+++ application_processor/inc/ecdsatest.h
@@ -0,0 +1,6 @@
+#include <ucl_testing_config.h>
+#include <ecdsa_generic_api.h>
+#include <ucl_hash.h>
+
+int test_ecdsa_curves(void);
+
--- application_processor/project.mk
+++ application_processor/project.mk
@@ -39,3 +39,65 @@ CRYPTO_EXAMPLE=0
 
 # Enable Crypto Example
 #CRYPTO_EXAMPLE=1
+
+#Adds the periph driver to the build from the periphdriver.mk file
+
+
+ifeq "$(PERIPH_DRIVER_DIR)" ""
+$(error "PERIPH_DRIVER_DIR must be specified")
+endif
+
+TARGET_UC:=$(shell echo $(TARGET) | tr a-z A-Z)
+TARGET_LC:=$(shell echo $(TARGET) | tr A-Z a-z)
+
+# Specify the library variant.
+ifeq "$(MFLOAT_FLAGS)" "hardfp"
+LIBRARY_VARIANT=hardfp
+else
+ifeq "$(MFLOAT_FLAGS)" "hard"
+LIBRARY_VARIANT=hardfp
+else
+LIBRARY_VARIANT=softfp
+endif
+endif
+
+# Specify the build directory if not defined by the project
+ifeq "$(BUILD_DIR)" ""
+ifeq "$(RISCV_CORE)" ""
+PERIPH_DRIVER_BUILD_DIR=${PERIPH_DRIVER_DIR}/bin/$(TARGET_UC)/$(LIBRARY_VARIANT)
+else
+PERIPH_DRIVER_BUILD_DIR=${PERIPH_DRIVER_DIR}/bin/$(TARGET_UC)/$(LIBRARY_VARIANT)_riscv
+endif
+else
+PERIPH_DRIVER_BUILD_DIR=$(BUILD_DIR)/PeriphDriver
+endif
+
+# Export other variables needed by the peripheral driver makefile
+export TARGET
+export COMPILER
+export TARGET_MAKEFILE
+export PROJ_CFLAGS
+export PROJ_LDFLAGS
+export MXC_OPTIMIZE_CFLAGS
+export DUAL_CORE
+export RISCV_CORE
+
+include ${PERIPH_DRIVER_DIR}/$(TARGET_LC)_files.mk
+IPATH += ${PERIPH_DRIVER_INCLUDE_DIR}
+ifeq "$(LIBRARY_VARIANT)" ""
+PERIPH_DRIVER_LIB := libPeriphDriver.a
+else
+PERIPH_DRIVER_LIB := libPeriphDriver_$(LIBRARY_VARIANT).a
+endif
+# export PERIPH_DRIVER_DIR
+export PERIPH_DRIVER_LIB
+export PERIPH_DRIVER_BUILD_DIR
+
+# Add to library list
+LIBS += ${PERIPH_DRIVER_BUILD_DIR}/${PERIPH_DRIVER_LIB}
+# Add rule to build the Driver Library
+${PERIPH_DRIVER_BUILD_DIR}/${PERIPH_DRIVER_LIB}: ${PERIPH_DRIVER_C_FILES} ${PERIPH_DRIVER_A_FILES} ${PERIPH_DRIVER_H_FILES}
+	$(MAKE) -f ${PERIPH_DRIVER_DIR}/libPeriphDriver.mk  lib BUILD_DIR=${PERIPH_DRIVER_BUILD_DIR} 
+
+clean.periph:
+	@rm -rf ${PERIPH_DRIVER_BUILD_DIR}/*
