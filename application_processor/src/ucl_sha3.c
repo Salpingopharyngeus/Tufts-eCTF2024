d58dea5e6963a97a451cf0779a93a92a2aca7b8c
Jfreedman501 <98427898+Jfreedman501@users.noreply.github.com>
2024-02-18 14:03:09 -0500

    Update ucl_sha3.c

--- application_processor/src/ucl_sha3.c
+++ application_processor/src/ucl_sha3.c
@@ -58,7 +58,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
  
-#include <ucl/ucl_hash.h>
+#include <ucl_hash.h>
 #ifdef HASH_SHA3
 #include <string.h>
 #include <ucl_config.h>
