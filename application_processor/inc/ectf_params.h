39d76d7a9d4e692e772ed2625d0931bcef23d8cd
Sam Chung <chung.woonyong@gmail.com>
2024-03-03 00:50:33 -0500

    revert back to when everything was working

--- application_processor/inc/ectf_params.h
+++ application_processor/inc/ectf_params.h
@@ -1,7 +1,7 @@
 #ifndef __ECTF_PARAMS__
 #define __ECTF_PARAMS__
-#define AP_PIN "$2b$07$eMWSdxGmQMbvtSJR9658JOlq0cJzKL.sbxXdISiGHp94NVAsyL70."
-#define AP_TOKEN "$2b$07$JMhhV/Oe8DYrF2eM.w32COwcxumyb3uheFXgrOPRo4p6C.5lYbBKS"
+#define AP_PIN "$2b$07$u52Iu1aNeqH0JEEoZPuoneTqC4bWb944Tp9f0T7hNtrQAZ0/PKr7G"
+#define AP_TOKEN "$2b$07$0t7L6WS5YwR5Bp7hkwUoZOo0fcTyCMhUefwNm347xCAi5eiZmwF2O"
 #define COMPONENT_IDS 0x11111124, 0x11111125
 #define COMPONENT_CNT 2
 #define AP_BOOT_MSG "Test boot message"
