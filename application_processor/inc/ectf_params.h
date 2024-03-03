0d1282f37c72c35ec605a96f32bbe89e58e14380
Sam Chung <chung.woonyong@gmail.com>
2024-03-03 01:50:02 -0500

    add correct mdsk that works for both Jakes branch and software team one

--- application_processor/inc/ectf_params.h
+++ application_processor/inc/ectf_params.h
@@ -1,7 +1,7 @@
 #ifndef __ECTF_PARAMS__
 #define __ECTF_PARAMS__
-#define AP_PIN "$2b$07$u52Iu1aNeqH0JEEoZPuoneTqC4bWb944Tp9f0T7hNtrQAZ0/PKr7G"
-#define AP_TOKEN "$2b$07$0t7L6WS5YwR5Bp7hkwUoZOo0fcTyCMhUefwNm347xCAi5eiZmwF2O"
+#define AP_PIN "$2b$07$FWBNxADCBdjuLUzGi6C3de8n/lvGJNlY3Vg7aLP2O0Quk7lcpetO6"
+#define AP_TOKEN "$2b$07$doxE1o1P8AAFYYOL1oGGEeHbNmu7dH9aYxVGTABa69bfROogmDuHO"
 #define COMPONENT_IDS 0x11111124, 0x11111125
 #define COMPONENT_CNT 2
 #define AP_BOOT_MSG "Test boot message"
