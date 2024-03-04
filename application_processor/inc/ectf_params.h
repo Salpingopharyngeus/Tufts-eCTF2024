91885047b6fbcb7c0deb4b1c0092c722e5a49192
Sam Chung <chung.woonyong@gmail.com>
2024-03-03 19:30:09 -0500

    latest working verison of branch

--- application_processor/inc/ectf_params.h
+++ application_processor/inc/ectf_params.h
@@ -1,7 +1,7 @@
 #ifndef __ECTF_PARAMS__
 #define __ECTF_PARAMS__
-#define AP_PIN "$2b$07$FWBNxADCBdjuLUzGi6C3de8n/lvGJNlY3Vg7aLP2O0Quk7lcpetO6"
-#define AP_TOKEN "$2b$07$doxE1o1P8AAFYYOL1oGGEeHbNmu7dH9aYxVGTABa69bfROogmDuHO"
+#define AP_PIN "$2b$07$38f7A366i1hNecZ5toYhZuOSmKcnu6Jr080ij6058cn54EXj0jOme"
+#define AP_TOKEN "$2b$07$RTiLK6eIj.S1Qfhbz3RLdODCBgF5yLmf9pX9xhJiUNi.Ln8qFV2Uy"
 #define COMPONENT_IDS 0x11111124, 0x11111125
 #define COMPONENT_CNT 2
 #define AP_BOOT_MSG "Test boot message"
