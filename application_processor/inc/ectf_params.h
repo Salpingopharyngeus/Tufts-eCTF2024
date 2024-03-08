484c45a02140691a9e69b7235e94eef5768a646e
Kamran Mirza <101909497+kmirza7@users.noreply.github.com>
2024-03-07 20:12:03 -0500

    update run script

--- application_processor/inc/ectf_params.h
+++ application_processor/inc/ectf_params.h
@@ -1,7 +1,7 @@
 #ifndef __ECTF_PARAMS__
 #define __ECTF_PARAMS__
-#define AP_PIN "$2b$07$EOy5mUrwDrbz.X794sLzwuGsKVGP.CQl9ZN.RKcXKUArX5RRsdapG"
-#define AP_TOKEN "$2b$07$q1vxGWRBabIHj7WxAz6YFuNeEQBhX74OQkQrCwZBvmudhRzsUgnf6"
+#define AP_PIN "$2b$07$FagjVDbB9UG/7HDcZYV56uPptuKkP/XB.sU.gCrR2yuzWaj88yOI."
+#define AP_TOKEN "$2b$07$qRKrlp3ti6oZkbOpnGJrCOPnW4SxMbtRz1sxl4c64RoBoodEY7mKi"
 #define COMPONENT_IDS 0x11111124, 0x11111125
 #define COMPONENT_CNT 2
 #define AP_BOOT_MSG "Test boot message"
