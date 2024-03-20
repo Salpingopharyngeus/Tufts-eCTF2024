c1b9f4fd7068a7b769ed48f8ca1ce300d0fb5895
Sam Chung <chung.woonyong@gmail.com>
2024-03-20 15:10:36 -0400

    more debugging

--- application_processor/src/application_processor.c
+++ application_processor/src/application_processor.c
@@ -348,7 +348,7 @@ int secure_send(uint8_t address, uint8_t* buffer, uint8_t len) {
     // Update random number assignment for component
     addOrUpdate(&dict, address, random_number);
 
-    print_info("RANDOM NUMBER ASSIGNED TO 0x%08x: %u", address, random_number);
+    print_info("RANDOM NUMBER ASSIGNED TO 0x%02X: %u", address, random_number);
 
     // Send the packet
     return send_packet(address, MAX_PACKET_SIZE, temp_buffer);
@@ -366,6 +366,7 @@ int secure_send(uint8_t address, uint8_t* buffer, uint8_t len) {
  * This function must be implemented by your team to align with the security requirements.
 */
 int secure_receive(i2c_addr_t address, uint8_t* buffer) {
+    print_info("SECURE RECEIVED PACKET FROM COMPONENT!");
     size_t MAX_PACKET_SIZE = MAX_I2C_MESSAGE_LEN - 1;
 
     uint8_t len = poll_and_receive_packet(address, buffer); 
@@ -373,7 +374,8 @@ int secure_receive(i2c_addr_t address, uint8_t* buffer) {
     // Extract the random number
     uint32_t random_number;
     memcpy(&random_number, buffer + MAX_PACKET_SIZE - sizeof(uint32_t), sizeof(uint32_t));
-
+    print_info("RECEIVED RANDOM NUMBER: %u", random_number);
+    
     int seen = searchUint32Buffer(random_number_hist, random_number);
     if(seen){
         print_error("ERROR: POTENTIAL REPLAY ATTACK!");
@@ -410,10 +412,11 @@ int secure_receive(i2c_addr_t address, uint8_t* buffer) {
 
     // Check hash for integrity and authenticity of the message
     if(!hash_equal(received_hash, check_hash) || random_number != getValue(&dict, address)){
+        print("INVALID PACKET FROM COMPONENT!");
         for (unsigned i = 0; i < flash_status.component_cnt; i++) {
             print_info("P>0x%08x\n", flash_status.component_ids[i]);
         }
-        print_info("address: 0x%08x\n", address);
+        print_info("address: 0x%02X\n", address);
         print_info("EXPECTED RANDOM NUMBER: %u", getValue(&dict, address));
         print_info("RECEIVED RANDOM NUMBER: %u", random_number);
         print_info("\nRECEIVED HASH: ");
